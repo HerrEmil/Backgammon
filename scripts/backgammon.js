bfbb08c490da17567f7f7d40930fb0643e01bc55
Emil Andersson <vaccuum@gmail.com>
2013-01-27 20:03:39 +0100

    asdf

---
+++ scripts/backgammon.js
@@ -0,0 +1,7 @@
+/*
+Four lanes
+Each lane has a bunch of positions
+Each position contains 0 - 5 pieces of either color, one color at a time
+Pieces move in one direction, up or down, depending on team
+
+*/
