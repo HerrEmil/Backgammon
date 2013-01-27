94e3da6814bdf0d221291e4edcd8b4811b65b92d
Emil Andersson <vaccuum@gmail.com>
2013-01-27 22:56:09 +0100

    Added makeMove function, not finished

--- scripts/class_boardlane.js
+++ scripts/class_boardlane.js
@@ -1,8 +1,8 @@
-// BoardPane is a vertical slide of the game area, 4 instances,
+// BoardPane is a vertical slice of the game area, 4 instances,
 // instances not directly affected by each other
 
 function BoardLane() {
-	"use strict";
+	'use strict';
 	/*
 		Each lane has 24 positions
 		Each position contains 0 - 5 pieces of either color, one color at a time
@@ -13,64 +13,79 @@ function BoardLane() {
 	var i;
 	this.positions = [];
 	for (i = 0; i < 24; i += 1) {
-		this.positions[i] = ['-'];
+		this.positions[i] = [0, '-'];
 	}
 	// Red pieces are R, black pieces are B
-	this.positions[0][0] = 'R';
-	this.positions[0][1] = 'R';
-
-	this.positions[5][0] = 'B';
-	this.positions[5][1] = 'B';
-	this.positions[5][2] = 'B';
-	this.positions[5][3] = 'B';
-	this.positions[5][4] = 'B';
-
-	this.positions[7][0] = 'B';
-	this.positions[7][1] = 'B';
-	this.positions[7][2] = 'B';
-
-	this.positions[11][0] = 'R';
-	this.positions[11][1] = 'R';
-	this.positions[11][2] = 'R';
-	this.positions[11][3] = 'R';
-	this.positions[11][4] = 'R';
-
-	this.positions[12][0] = 'B';
-	this.positions[12][1] = 'B';
-	this.positions[12][2] = 'B';
-	this.positions[12][3] = 'B';
-	this.positions[12][4] = 'B';
-
-	this.positions[16][0] = 'R';
-	this.positions[16][1] = 'R';
-	this.positions[16][2] = 'R';
-
-	this.positions[18][0] = 'R';
-	this.positions[18][1] = 'R';
-	this.positions[18][2] = 'R';
-	this.positions[18][3] = 'R';
-	this.positions[18][4] = 'R';
-
-	this.positions[23][0] = 'B';
-	this.positions[23][1] = 'B';
+	this.positions[0] = [2, 'R'];
+	this.positions[5] = [5, 'B'];
+	this.positions[7] = [3, 'B'];
+	this.positions[11] = [5, 'R'];
+	this.positions[12] = [6, 'B'];
+	this.positions[16] = [3, 'R'];
+	this.positions[18] = [5, 'R'];
+	this.positions[23] = [2, 'B'];
 }
+
+// Function to move pieces
+BoardLane.prototype.makeMove = function (color, position) {
+	'use strict';
+	var direction,
+		nextPosition;
+
+	// Pick direction the piece should be moved based on color
+	if (color === 'R') {
+		direction = 1;
+	} else if (color === 'B') {
+		direction = -1;
+	} else {
+		console.error("makeMove was called with the invalid color: " + color);
+		return;
+	}
+
+	nextPosition = position + direction;
+
+	// Check if the position and piece exists
+	if (position > -1 && position < 24 && this.positions[position][1] === color) {
+		// To do: Check if the next position is blocked by opponent
+		// Remove the piece from the old position
+		this.positions[position][0] -= 1;
+
+		// If the old position is empty, change color
+		if (this.positions[position][0] === 0) {
+			this.positions[position][1] = '-';
+		}
+
+		// This is assuming that the next position does not contain opponent piece
+		// which is WRONG
+		this.positions[nextPosition][0] += 1;
+		this.positions[nextPosition][1] = color;
+	} else {
+		console.error("The position " + (position + 1) + " does not have a " + color + " piece!");
+	}
+};
 
 BoardLane.prototype.getOccupiedPositions = function () {
   // Stuff
-	"use strict";
+	'use strict';
+};
+
+// Function for AI later on
+BoardLane.prototype.getAllValidMoves = function (color) {
+	// return all possible moves for player with color
+	'use strict';
 };
 
 // Helper function until we have something to show on-screen
 BoardLane.prototype.printPane = function () {
-	"use strict";
+	'use strict';
 	var i,
 		j,
-		currentPosition = "";
+		currentPosition = '';
 
 	for (i = 0; i < 24; i += 1) {
-		currentPosition = "";
-		for (j = 0; j < this.positions[i].length; j += 1) {
-			currentPosition += this.positions[i][j];
+		currentPosition = '';
+		for (j = 0; j < this.positions[i][0]; j += 1) {
+			currentPosition += this.positions[i][1];
 		}
 		console.log("Position " + (i + 1) + ": " + currentPosition);
 	}
